2ed252f211de982a9ab0efc756945e5fab71af68
RaulStanciu1 <stanciuraul405@gmail.com>
2026-04-19 19:10:57 +0300

    Added Maven Spring Boot project template

---
+++ backend/mys/src/main/java/com/electroghiurai/mys/MysApplication.java
@@ -0,0 +1,13 @@
+package com.electroghiurai.mys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysApplication.class, args);
+	}
+
+}
---
+++ backend/mys/src/test/java/com/electroghiurai/mys/MysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electroghiurai.mys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
